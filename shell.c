217646953f7f3c4e971df2270ae081824c612b80
ssssssssssong <84441235+ssssssssssong@users.noreply.github.com>
2021-05-23 16:46:44 +0900

    Update shell.c

--- shell.c
+++ shell.c
@@ -5,8 +5,11 @@
 #include <unistd.h>
 #include <sys/types.h>
 #include <sys/wait.h>
+#include <time.h>
+#include <pwd.h>
 
 #define MAX_LEN_LINE    100
+#define LEN_HOSTNAME	30
 
 int main(void)
 {
@@ -14,18 +17,85 @@ int main(void)
     char *args[] = {command, NULL};
     int ret, status;
     pid_t pid, cpid;
+    char * a[12];
+    printf("                         \n");
+    a[0]=" ・。゜ ・・。゜ ・・。゜ ・・。゜ ・・。゜ ・・。゜ ・・。゜ ・・。゜ ・ ・。゜ ・・。゜ ・";
+    a[1]="゜・。゜ ・ 。。゜°・。 •。。・°。゜・。゜ ・ 。。゜°・。 •。。・°。゜・。゜ ・ 。。゜°・゜・ 。゜゜゜ 。   ";
+    a[2]=" ゜・。゜・ 。゜ ・ 。。゜・。。 。・。゜・ 。゜゜゜ 。゜・。゜・ 。゜ ・ 。。゜・。。 。・゜・ 。゜゜゜ 。  ";
+    a[3]=" ゜・。゜・ 。゜ ・ 。。゜・。。 。・。゜・ 。゜゜゜ 。゜・。゜・ 。゜ ・ 。。゜・。。 。・。゜・ 。゜゜゜   ";    
+    a[4]="゜・。゜・ 。゜ ・ 。。゜・。。 。・。゜・ 。゜゜゜ 。゜・。゜・ 。゜ ・ 。。゜・。。 。・。゜・ 。゜゜゜ 。";
+    a[5]=" 。゜ ・ 。。゜・。。・。゜ ・。゜ ・ ゜・。。゜ ・ 。。゜・。。・。゜ ・。゜ ・ ゜・。。゜ ・ 。。゜・。。・ ";
+    a[6]=" ・。゜ ・ 。。゜°・。 •。。・°・。゜ ・ 。。゜°・。 •。。・°・。゜ ・ 。。゜°・。 •。。・°゜・ 。゜゜゜ 。  ";
+
+    a[7]="・。゜ ・ 。。゜°・。 •。。・°・。゜ ・ 。。゜°・。 •。。・°・。゜ ・ 。。゜°・。 •。。・°・。゜ ・ 。。゜°・°";
+    a[8]=" 。 。・。゜・ 。゜゜゜ 。。 。・。゜・ 。゜゜゜ 。。 。・。゜・ 。゜゜゜ 。。 。・。゜・ 。゜゜゜ 。。 。・。゜";
+    a[9]=" 。゜ ・ 。。゜・。。・。゜ ・。゜ ・ ゜・。 ゜。゜ ・ 。。゜・。。・。゜ ・。゜ ・ ゜・。 ゜。゜ ・ 。。゜・。 ";
+
+    a[10]=" ...。。゜・。。 ...。。゜・。。 ...。。゜・。。 ...。。゜・。。 ...。。゜・。。 ...。。゜・。。 ...。。゜・。 ";
+
+    a[11]=" 。゜・ 。゜ ・ ゜・。 ゜・ 。゜・ 。゜・ 。。゜・。 。゜・ 。゜ ・ ゜・。 ゜・ 。゜・ 。゜・ 。。゜・。 。゜・ 。";
+    //a[12]="  。゜・ 。゜ ・ 。゜ 。゜・ 。゜ ・ 。゜ 。゜・ 。゜ ・ 。゜ 。゜・ 。゜ ・ 。゜ 。゜・ 。゜ ・ 。゜ 。゜・ 。゜ ・ 。゜ 。゜・ 。゜ ・ 。゜ 。゜・ 。゜ ・ 。゜";
+    //a[13]="  。゜・ 。゜ ・ 。゜ 。゜・ 。゜ ・ 。゜ 。゜・ 。゜ ・ 。゜ 。゜・ 。゜ ・ 。゜ 。゜・ 。゜ ・ 。゜ 。゜・ 。゜ ・ 。゜";
+    //a[14]=" ・ ゜・。 ゜・ 。゜・ 。゜・ ・ ゜・。 ゜・ 。゜・ 。゜・ ・ ゜・。 ゜・ 。゜・ 。゜・ ・ ゜・。 ゜・ 。゜・ 。゜・ ・ ゜・。 ゜・ 。゜・ 。゜・ ・ ゜・。 ゜・ 。゜・ 。゜・ ";
+    //a[15]=" ゜ ゜・。゜・ ゜ ゜・。゜・ ゜ ゜・。゜・ ゜ ゜・。゜・ ゜ ゜・。゜・ ゜ ゜・。゜・ ゜ ゜・。゜・ ゜ ゜・。゜・ ゜ ゜・。゜・ ゜ ゜・。゜・ ゜ ゜・。゜・ ゜ ゜・。゜・ ゜ ゜・。゜・   ";
+    //a[16]="  ゜ ゜・。゜・ ゜ ゜・。゜・ ゜ ゜・。゜・ ゜ ゜・。゜・ ゜ ゜・。゜・ ゜ ゜・。゜・ ゜ ゜・。゜・ ゜ ゜・。゜・ ゜ ゜・。゜・ \n";
+
+    int picture;
+    for(picture=0;picture<12;picture++){
+        printf("%s",a[picture]);
+        printf("\n");
+    };
     
+    char hostname[LEN_HOSTNAME + 1];
+    memset(hostname, 0x00, sizeof(hostname));
+    printf("\033[1;32m username:\033[0m \033[1;35m %s \033[0m \n", getpwuid(getuid())->pw_name);
+    
+    gethostname(hostname, LEN_HOSTNAME);
+    printf("\033[1;32m hostname:\033[0m \033[1;35m %s \033[0m \n", hostname);
+
+    
+    
+    struct tm *clock;
+    time_t current;
+
+
+    time(& current);
+
+    clock=localtime(& current); 
+
+    printf("현재시간 = %d시 %d분 %d초\n",clock->tm_hour%12,clock->tm_min,clock->tm_sec);
+    printf("\n");
+        
     while (true) {
+        
         char *s;
         int len;
         
-        printf("MyShell $ ");
+        printf("\033[1;34m SongShell $~\033[0m");
         s = fgets(command, MAX_LEN_LINE, stdin);
         if (s == NULL) {
             fprintf(stderr, "fgets failed\n");
             exit(1);
         }
+                       
+        command[strlen(command) - 1] = '\0'; 
+        if(!strcmp("exit",command)) {
+            printf("종료\n");
+            exit(1);
         
+            //break;
+            //return -1;
+        }
+        
+        if(strcmp(command,"ls") ==0){
+            system("ls");
+            continue;
+        }
+        
+        if(strcmp(command,"clear")==0){
+            system("clear");
+            continue;
+        }
         len = strlen(command);
         printf("%d\n", len);
         if (command[len - 1] == '\n') {
@@ -57,5 +127,6 @@ int main(void)
             }
         } 
     }
+
     return 0;
-}
+    }
